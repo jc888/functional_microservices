906e7951f4129ae6237052a624fcc82500ba8c30
jc888 <james@jc888.co.uk>
2016-10-15 12:23:30 +0100

    manager worker

--- talk3/manager.js
+++ talk3/manager.js
@@ -1,11 +1,32 @@
-var R = require('ramda');
-var Future = require('ramda-fantasy').Future;
-var RSMQWorker = require("rsmq-worker");
+const R = require('ramda');
+const { map, compose, pipe, curry, tap, chain, always } = require('ramda');
+const { Future } = require('ramda-fantasy');
+const RSMQWorker = require("rsmq-worker");
 
-worker = new RSMQWorker("myqueue", {
+const worker = new RSMQWorker("worker", {
     autostart: false
 });
 
-worker.on("ready", function() {
-    worker.send("gummy2");
+const onWorkerReady = (worker, data) =>
+    Future((reject, resolve) => worker.on('ready', () => resolve(data)));
+
+const workerSend = (worker, data) =>
+    Future((reject, resolve) => worker.send(data, null, (err, data) => err ? reject(err) : resolve(data)));
+
+const onReady = curry(onWorkerReady)(worker);
+const send = curry(workerSend)(worker);
+
+const sendPayload = pipe(
+    onReady,
+    map(JSON.stringify),
+    chain(send)
+);
+
+sendPayload({
+    type: "blah"
+}).fork(() => {}, () => {})
+
+worker.on("message", function(msg, next, id) {
+    console.log("RECEIVED", msg, id);
+    next();
 });
--- talk3/worker.js
+++ talk3/worker.js
@@ -1,12 +1,29 @@
-var RSMQWorker = require("rsmq-worker");
+const R = require('ramda');
+const { map, compose, pipe, curry, tap, chain, always, juxt, mergeAll, identity, prop, objOf, assoc,converge } = require('ramda');
+const { Future } = require('ramda-fantasy');
+const RSMQWorker = require("rsmq-worker");
 
-worker = new RSMQWorker("myqueue", {
-    interval: .1
+worker = new RSMQWorker("worker", {
+    interval: .1,
+    autostart: true
 });
+
+const strip = pipe(
+	prop('msg'),
+	JSON.parse
+)
+
+const process = pipe(
+    tap(val => val.next()),
+    juxt([identity,strip]),
+    mergeAll,
+    tap(console.log)
+)
 
 worker.on("message", function(msg, next, id) {
-    console.log("RECEIVED", msg,id);
-    next();
+    process({
+        msg: msg,
+        next: next,
+        id: id
+    })
 });
-
-worker.start();
