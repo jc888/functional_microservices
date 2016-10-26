553d5f7ef13d806346633aa02f37874c0aa62a65
jc888 <james@jc888.co.uk>
2016-10-26 17:59:12 +0100

    simplify even more

--- talk3/searchHandler.js
+++ talk3/searchHandler.js
@@ -1,57 +1,47 @@
 const { sequence, reduce, curry, merge, objOf, compose, map, chain, prop } = require('ramda');
 const { Future } = require('ramda-fantasy');
 
-var logger = curry((msg, v) => {
+const logger = curry((msg, v) => {
     console.log(msg, v);
     return v;
 });
 
-var elasticsearchConf = {
+const elasticsearchConf = {
     host: 'localhost:9200',
     log: 'error'
 };
 
-var mongoConf = {
+const mongoConf = {
     url: 'mongodb://localhost:27017/function_microservices'
 };
 
-var elasticsearch = require('elasticsearch');
-var elasticsearchClient = new elasticsearch.Client(elasticsearchConf);
+const elasticsearch = require('elasticsearch');
+const elasticsearchClient = new elasticsearch.Client(elasticsearchConf);
 
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-var mongoConnect = conf => Future((reject, resolve) => MongoClient.connect(conf.url, (err, db) => err ? reject(err) : resolve(db)))
-var mongoCollection = curry((collectionName, db) => db.collection(collectionName));
-var findOne = curry((query, collection) => Future((reject, resolve) => collection.findOne(query, {}, (err, result) => err ? reject(err) : resolve(result))));
+const mongoConnect = conf => Future((reject, resolve) => MongoClient.connect(conf.url, (err, db) => err ? reject(err) : resolve(db)))
+const mongoCollection = curry((collectionName, db) => db.collection(collectionName));
+const findOne = curry((query, collection) => Future((reject, resolve) => collection.findOne(query, {}, (err, result) => err ? reject(err) : resolve(result))));
 
-var search = query => Future((reject, resolve) => elasticsearchClient.search(query).then(resolve, reject));
-var parseSearch = compose(map(prop('_source')), prop('hits'), prop('hits'))
-var findTalks = compose(map(parseSearch), search);
-
-var find = curry((url, collectionName, query) => compose(
+const find = curry((url, collectionName, query) => compose(
     chain(findOne(query)),
     map(mongoCollection(collectionName)),
     mongoConnect
-)(url))
+)(url));
 
-var findSpeaker = find(mongoConf, 'speakers');
+const search = query => Future((reject, resolve) => elasticsearchClient.search(query).then(resolve, reject));
+const parseSearch = compose(map(prop('_source')), prop('hits'), prop('hits'))
+const findTalks = compose(map(parseSearch), search);
 
-var queryFromTalk = compose(objOf('name'), prop('speaker'))
+const findSpeaker = find(mongoConf, 'speakers');
+const queryFromTalk = compose(objOf('name'), prop('speaker'));
+const findSpeakerForTalk = compose(findSpeaker, queryFromTalk);
+const findSpeakerAndJoin = talk => compose(map(merge(talk)), findSpeakerForTalk)(talk);
+const findSpeakerForEachTalk = compose(sequence(Future.of), map(findSpeakerAndJoin));
 
-var findSpeakerAndJoin = talk => compose(
-    map(merge(talk)),
-    map(logger('found speaker')),
-    findSpeaker,
-    queryFromTalk
-)(talk);
-
-var findSpeakerForEachTalk = compose(
-    sequence(Future.of),
-    map(findSpeakerAndJoin)
-);
-
-var searchHandler = compose(
+const searchHandler = compose(
     chain(findSpeakerForEachTalk),
     map(logger('search results : ')),
     findTalks,
