6a5934ac316ef435b75e9257389394fc3217b065
jc888 <james@jc888.co.uk>
2016-11-14 17:17:16 +0000

    cors required for lnug-client

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const handlers = require('./handlers');
+app.use(cors());
 
 app.get('/', function(req, res) {
     handlers.search(req.query)
         .fork(
             err => res.status(err.status || 500).send(err.message),
-            r => res.json(r)
+            r => res.send(JSON.stringify(r))
         )
 });
 
