422265004faef8b85addb5bc0cdfa66bd551cb17
jc888 <james@jc888.co.uk>
2016-11-23 14:57:29 +0000

    search2 self contained

--- handlers/search2.js
+++ handlers/search2.js
@@ -1,8 +1,8 @@
 // @flow
 const { indexBy, assoc, curry, compose, map, chain, prop, path, merge, pluck, head } = require('ramda');
-const logger = require('../lib/logger');
-const mongo = require('../mongo');
-const elasticsearch = require('../elasticsearch');
+const { MongoClient } = require('mongodb');
+const elasticsearch = require('elasticsearch');
+const Future = require('fluture');
 
 // innerJoin :: String -> String -> [a] -> [b] -> [{String:a}]
 const innerJoin = curry((k1, k2, l1, l2) => {
@@ -10,12 +10,18 @@ const innerJoin = curry((k1, k2, l1, l2) => {
     return map(l1v => assoc(k1, list2Map[l1v[k1]], l1v), l1)
 });
 
+// searchMongo :: SpeakerQuery -> Future Error [Speaker]
+const searchMongo = query => Future((reject, resolve) => {
+    MongoClient.connect('mongodb://mongo:27017/function_microservices')
+        .then(db => db.collection('speakers'))
+        .then(coll => coll.find(query).toArray())
+        .then(resolve)
+        .catch(reject);
+});
+
 // mongoQueryFromTalks :: [Talk] -> SpeakerQuery
 const mongoQueryFromTalks = compose(speakers =>
     ({ handle: { $in: speakers } }), pluck('speaker'));
-
-// searchMongo :: SpeakerQuery -> Future Error [Speaker]
-const searchMongo = mongo.find('speakers');
 
 // findSpeakersFromTalks :: [Talk] -> Future Error [Speaker]
 const findSpeakersFromTalks = compose(searchMongo, mongoQueryFromTalks);
@@ -24,18 +30,25 @@ const findSpeakersFromTalks = compose(searchMongo, mongoQueryFromTalks);
 const addSpeakers = talks =>
     compose(map(innerJoin('speaker', 'handle', talks)), findSpeakersFromTalks)(talks);
 
-// searchElasticSearch :: TalkQuery -> Future Error {hits:{hits:[{_source:Talk}]}}
-const searchElasticSearch = elasticsearch.search;
-
 // parseResults :: {hits:{hits:[{_source:Talk}]}} -> [Talk]
 const parseResults = compose(pluck('_source'), path(['hits', 'hits']));
+
+// searchElasticSearch :: TalkQuery -> Future Error {hits:{hits:[{_source:Talk}]}}
+const searchElasticSearch = query => Future((reject, resolve) => {
+    new elasticsearch.Client({
+            host: 'elasticsearch:9200',
+            log: 'error'
+        })
+        .search(query)
+        .then(resolve)
+        .catch(reject);
+});
 
 // findTalks :: TalkQuery -> Future Error [Talk]
 const findTalks = compose(map(parseResults), searchElasticSearch);
 
 // search :: TalkQuery -> Future Error [TalkEmbeddedWithSpeaker]
 const search = compose(chain(addSpeakers), findTalks);
-
 
 module.exports = {
     mongoQueryFromTalks,
